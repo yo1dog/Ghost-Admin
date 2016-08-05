2aaa5a9c10708ef90b666bdd0367042e5604e01d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2016-08-05 14:16:02 +0200

    ensure we our oauth redirect_uris have a trailing slash

--- app/torii-providers/ghost-oauth2.js
+++ app/torii-providers/ghost-oauth2.js
@@ -10,7 +10,7 @@ let GhostOauth2 = Oauth2.extend({
     responseParams: ['code'],
 
     // we want to redirect to the ghost admin app by default
-    redirectUri: window.location.href.replace(/(\/ghost)(.*)/, '$1'),
+    redirectUri: window.location.href.replace(/(\/ghost)(.*)/, '$1/'),
 
     open(options) {
         if (options.type) {
