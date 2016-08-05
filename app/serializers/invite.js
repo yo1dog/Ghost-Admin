d19ef8639c6aef871cdd2137ac758265bc312665
Kevin Ansfield <kevin@lookingsideways.co.uk>
2016-08-05 12:43:40 +0200

    fix invite serialization to account for embedded roles

--- app/serializers/invite.js
+++ app/serializers/invite.js
@@ -1,7 +1,9 @@
 import ApplicationSerializer from 'ghost-admin/serializers/application';
+import EmbeddedRecordsMixin from 'ember-data/serializers/embedded-records-mixin';
 
-export default ApplicationSerializer.extend({
+export default ApplicationSerializer.extend(EmbeddedRecordsMixin, {
     attrs: {
+        roles: {embedded: 'always'},
         createdAtUTC: {key: 'created_at'},
         updatedAtUTC: {key: 'updated_at'}
     }
